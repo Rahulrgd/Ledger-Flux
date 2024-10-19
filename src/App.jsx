e8e7d652eb4879638b99cfe8112a3483b5407f5a
RahulGupta <rahulrgd21@gmail.com>
2024-10-19 11:25:30 +0530

    Successfully configured tailwind in react app

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,9 @@ import './App.css';
 function App() {
   return (
     <div>
-      <h1>Ledger Flux Application</h1>
+      <h1 className='text-blue-700 text-4xl font-bold'>Ledger Flux Application</h1>
+      <p className='text-green-900 bg-red-600'>This is awesome paragraph</p>
+      <h1 className='text-blue-700 text-4xl font-bold'>Header one</h1>
     </div>
   );
 }
